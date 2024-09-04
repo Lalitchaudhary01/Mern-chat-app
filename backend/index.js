865f9a720f45f55ff48cecab41a0eeb4e52a46bc
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-09-04 12:48:17 +0530

    complete get other user

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 const corsOption = {
-  origin: "http://localhost:3002",
+  origin: "http://localhost:3001",
   credentials: true,
   optionSuccessStatus: 200,
 };
