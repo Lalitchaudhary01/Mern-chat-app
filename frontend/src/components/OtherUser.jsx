136717973b6c2eb8a209c5311a47800389471f0c
Lalitchdhary01 <Lalitkumar46789@gmail.com>
2024-09-05 23:33:17 +0530

    add one more custom hook

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectedUser } from '../redux/userSlice';
 
-const OtherUser = (props) => {
-  const user = props.user;
+const OtherUser = ({user}) => {
+  const dispatch = useDispatch();
+  const {selectedUser} = useSelector(store=>store.user);
+
+  
+  const selectedUserHandler = (user) => {
+        
+    dispatch(setSelectedUser(user)); 
+  }
   return (
-    <div>
-       <div className='flex gap-2 items-center text-white hover:bg-zinc-900 hover:bg-zinc-200 rounded p-2 cursor-pointer'>
+    <>
+       <div onClick={() => selectedUserHandler(user)} className={` ${selectedUser?._id === user?._id ? 'bg-zinc-200 text-black' : 'text-white'} flex gap-2 hover:text-black items-center hover:bg-zinc-200 rounded p-2 cursor-pointer`}>
         <div className='avatar online'>
           <div className='w-12 rounded-full'>
             <img src={user?.profilePhoto} alt='' />
@@ -18,7 +27,7 @@ const OtherUser = (props) => {
         </div>
       </div>
       <div className='divider my-0 py-0 h-1'></div>
-     </div>
+     </>
   )
 }
 
---
+++ frontend/src/hooks/useGetMessages.jsx
@@ -0,0 +1,25 @@
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+
+const useGetMessages = async () => {
+    const {selectedUser} = useSelector(store=>store.user);
+    useEffect(() => { 
+        const fetchMessages = async () => {
+            try {
+                axios.defaults.withCredentials = true;
+                const res = await axios.get(`http://localhost:8080/api/v1/message/${selectedUser._id}`);
+                console.log(res);
+
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchMessages();
+    }
+    , []);
+  
+}
+
+export default useGetMessages
